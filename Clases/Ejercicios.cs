1649dbca7b3e42728d2110a6fe051705b8c65cff
MariaLimon <limon185mari@gmail.com>
2023-10-03 22:18:05 -0700

    ejercicio 1,2,3 listos

--- Clases/Ejercicios.cs
+++ Clases/Ejercicios.cs
@@ -27,7 +27,32 @@ namespace EjerciciosED_03_10_23.Clases
 
         public void Ejercicio3()
         {
-            
+            int suma = 0;
+            int[] numeros = new int[10];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("dame un numero");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                suma += numeros[i]; 
+            }
+            double media = suma /numeros.Length;
+            Console.WriteLine($"la media es: {media}");
+
+        }
+
+        public void Ejercicio4(int tamaño, int numero)
+        {
+            int[] numerosMultiplos = new int[tamaño];
+
+            for(int i = 0; i < numerosMultiplos.Length;)
+            {
+                if (numero % i == 0)
+                {
+                    i++;
+                    numerosMultiplos[i] = i;
+                }
+            }
+            Console.WriteLine(string.Join(", ", numerosMultiplos));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,8 +9,19 @@ namespace EjerciciosED_03_10_23
             //Ejercicios ejercicio1 = new Ejercicios();
             //ejercicio1.Ejercicio1();
 
-            Ejercicios ejercicio2 = new Ejercicios();
-            ejercicio2.Ejercicio2();
+            //Ejercicios ejercicio2 = new Ejercicios();
+            //ejercicio2.Ejercicio2();
+
+            //Ejercicios ejercicios3 = new Ejercicios();
+            //ejercicios3 .Ejercicio3();
+
+            Console.WriteLine("dame el tamaño del arreglo");
+            int tamaño = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dame un numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Ejercicios ejercicios4 = new Ejercicios();
+            ejercicios4.Ejercicio4(tamaño, numero);
+
         }
     }
 }
